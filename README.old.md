e65a8ccaea4b915cf2c948c6edfb533548047ba2
Vishal <leo3369706@gmail.com>
2023-09-09 01:20:54 +0530

    Created search components

---
+++ README.old.md
@@ -0,0 +1,2 @@
+# ClimateCheck
+A weather app using react Js and other technologies with api integration user can check weather of any location by searching for it
---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Weather_index from './Components/weather-app/Weather_index';
+
+function App() {
+  return (
+    <div className="App">
+      <Weather_index/>
+    </div>
+  );
+}
+
+export default App;
